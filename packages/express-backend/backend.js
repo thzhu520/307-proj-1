0afff662f19292a6cb1a7a9b07ccd770f0f14933
Nathan Lee <nathan.lee5459@gmail.com>
2024-11-03 20:22:23 -0800

    added cors

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,14 +1,13 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(
