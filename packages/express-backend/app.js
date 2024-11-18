61f1a280e9ca8303e03df97460eb4fdd6f97f7ef
DerpNPurp <vincedoanschool@gmail.com>
2024-11-17 21:39:09 -0800

    updated app.js port number

--- packages/express-backend/app.js
+++ packages/express-backend/app.js
@@ -7,7 +7,7 @@ import cors from 'cors'; // Import CORS
 dotenv.config({ path: './packages/express-backend/.env' });
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware to parse JSON
 app.use(bodyParser.json());
